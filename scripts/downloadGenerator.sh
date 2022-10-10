625a9230fcab9b68bdbd1685b5657bdfdd63bbde
Paul Farault <paul.farault@gmail.com>
2022-10-10 14:46:28 +0200

    chore: openapi client sdk generator script

---
+++ scripts/downloadGenerator.sh
@@ -0,0 +1,6 @@
+OPENAPI_JAR_URL=https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.2.0/openapi-generator-cli-6.2.0.jar
+
+echo "Downloading $OPENAPI_JAR_URL"
+wget $OPENAPI_JAR_URL -O openapi-generator-cli.jar
+echo "Generating the API client..."
+java -jar openapi-generator-cli.jar generate -i schemas/tdp-server_0.1.0_openapi.json -g typescript-axios -o build/tdp-sdk
