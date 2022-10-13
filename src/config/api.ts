eb418b7561ff741d1d89c232d8a79344c9a84477
Paul Farault <diode-farault.consultant@dgfip.fincances.gouv.fr>
2022-10-13 13:22:02 +0200

    feat: client-sdk wrapper

---
+++ src/config/api.ts
@@ -0,0 +1,9 @@
+type ConfigType = {
+  apiBasePath: string
+}
+
+const config: ConfigType = {
+  apiBasePath: 'http://localhost:8000',
+}
+
+export default config
---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+import type {
+  DefaultApi,
+  ComponentsApi,
+  DeployApi,
+  ServicesApi,
+} from '@/client-sdk'
+
+export type TdpClientType = {
+  defaultApi: DefaultApi
+  componentsApi: ComponentsApi
+  deployApi: DeployApi
+  servicesApi: ServicesApi
+}
