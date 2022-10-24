2a718d53d9fcc09481ffcd7739c4ce653108ae13
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2022-10-24 11:54:17 +0200

    refactor: auth context provider and config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,7 @@ type ApiConfigType = {
   apiBasePath: string
   oidcDiscoveryUrl: string
   oidcClientId: string
+  redirectUri: string
 }
 
 const apiConfig: ApiConfigType = {
@@ -9,7 +10,8 @@ const apiConfig: ApiConfigType = {
   oidcDiscoveryUrl:
     process.env.OPENID_CONNECT_DISCOVERY_URL ||
     'http://localhost:8080/auth/realms/tdp_server_dev/.well-known/openid-configuration',
-  oidcClientId: process.env.OPENID_CLIENT_ID || 'tdp_server_id',
+  oidcClientId: process.env.OPENID_CLIENT_ID || 'tdp_auth',
+  redirectUri: 'http://localhost:3000/login',
 }
 
 export default apiConfig
