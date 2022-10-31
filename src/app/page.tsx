0e3d07e2b4eeac569a2becfae2f61dccdd5e66b7
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2022-10-31 18:53:01 +0100

    refactor: simplify context and services definition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 import Link from 'next/link'
+import { useServerStatus } from 'src/hooks'
 
 export default function App() {
+  const isServerRunning = useServerStatus()
   return (
     <>
-      <h1>Login page</h1>
+      <h1 className="font-black text-lg">Login page</h1>
+      <div className="flex gap-2">
+        <p>tdp-server running: </p>
+        <pre>{JSON.stringify(isServerRunning, null, 2)}</pre>
+      </div>
+
       <Link href="/dashboard" className="text-blue-800 underline">
         To the dashboard
       </Link>
