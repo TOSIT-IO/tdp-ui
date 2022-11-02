05763fcb50bae5544121835c935474352f808e55
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2022-11-02 12:27:14 +0100

    fix: only one call to the api

--- src/hooks/useServicesList.ts
+++ src/hooks/useServicesList.ts
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useTdpClient } from 'src/contexts'
 
+// TODO: make it an async component with a loading state
 export function useServicesList() {
   const { servicesApi } = useTdpClient()
   const [servicesList, setServicesList] = useState([])
 
-  async function fetchServicesList() {
-    const res = await servicesApi.getServicesApiV1ServiceGet()
-    setServicesList(res.data.map((service) => service.id))
-  }
-  fetchServicesList()
+  useEffect(() => {
+    async function fetchServicesList() {
+      const res = await servicesApi.getServicesApiV1ServiceGet()
+      console.log('Fetch services')
+      setServicesList(res.data.map((service) => service.id))
+    }
+    fetchServicesList()
+  }, [servicesApi])
 
   return servicesList
 }
