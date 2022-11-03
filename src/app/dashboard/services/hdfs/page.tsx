988f79900bf80e82a49dd9f8acb194b1d22ac941
Jean Savy <jean.savy@dgfip.finances.gouv.fr>
2022-11-03 18:33:00 +0100

    feat: hdfs service 5

--- src/app/dashboard/services/hdfs/page.tsx
+++ src/app/dashboard/services/hdfs/page.tsx
@@ -11,7 +11,7 @@ export default function HdfsPage() {
     // <div className="box-border p-4 border-4 basis-60 flex-grow h-screen w-auto">
     <div className="box-border m-auto p-4 border-4 basis-60 flex-grow-0 h-screen w-auto">
       {/* <pre className="overflow-auto flex-grow h-screen w-auto"> */}
-      <pre className="overflow-auto height: 80vh w-auto">
+      <pre className="overflow-auto flex-grow h-screen w-auto">
         {JSON.stringify(hdfsVariables, null, 2)}
       </pre>
     </div>
