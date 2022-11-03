d7ed408fbe90b7e53216a33b5c56185b5620e838
Jean Savy <jean.savy@dgfip.finances.gouv.fr>
2022-11-03 10:14:26 +0100

    feat: hdfs service 3

--- src/app/dashboard/services/hdfs/page.tsx
+++ src/app/dashboard/services/hdfs/page.tsx
@@ -7,8 +7,9 @@ export default function HdfsPage() {
   const hdfsVariables = useHdfsVariables()
 
   return (
-    <div className="box-border h-32 w-32 p-4 border-4">
-      <pre className="overflow-y-auto h-3/6">
+    //    <div className="box-border h-full w-screen p-4 border-4">
+    <div className="box-border p-4 border-4 basis-60 flex-grow h-screen w-auto">
+      <pre className="overflow-auto h-full w-full">
         {JSON.stringify(hdfsVariables, null, 2)}
       </pre>
     </div>
