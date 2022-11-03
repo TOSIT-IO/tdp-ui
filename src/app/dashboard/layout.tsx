7f3d2a60864df1884d9dccfbead54c2de3467ff7
Jean Savy <jean.savy@dgfip.finances.gouv.fr>
2022-11-03 15:09:55 +0100

    feat: hedf service 4

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,9 +2,11 @@ import Sidebar from './Sidebar'
 
 export default function DashboardLayout({ children }) {
   return (
-    <div className="flex">
+    <div className="flex m-1 space-x-1 h-screen">
       <Sidebar className="basis-60 flex-grow h-screen" />
-      <main className="basis-0 grow-[999]">{children}</main>
+      {/* <main className="basis-0 grow h-3/4 w-3/4">{children}</main> */}
+      {/* <main className="basis-0 flex-grow max-h-screen width:70vh">{children}</main> */}
+      <main className="basis-0 flex-grow max-h-screen  w-3/4">{children}</main>
     </div>
   )
 }
