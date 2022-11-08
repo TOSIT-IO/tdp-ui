9fc7b08d3fabc8f9d13a51bc28a584600d118bed
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2022-11-08 16:39:16 +0100

    refactor: disclosure component

---
+++ src/components/Disclosure.tsx
@@ -0,0 +1,36 @@
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid'
+import { useState } from 'react'
+import { classNames } from 'src/utils'
+
+type DisclosureType = {
+  title: string
+  children: JSX.Element
+}
+
+export function Disclosure({ title, children }: DisclosureType) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <>
+      <button
+        className="flex w-full justify-between rounded-md bg-slate-100 px-4 py-2 text-left text-sm font-medium text-slate-900"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <span>{title}</span>
+        {isOpen ? (
+          <ChevronUpIcon className="h-5 w-5" />
+        ) : (
+          <ChevronDownIcon className="h-5 w-5" />
+        )}
+      </button>
+      <div
+        className={classNames(
+          !isOpen && 'hidden',
+          'px-4 py-2 text-sm text-gray-500'
+        )}
+      >
+        {children}
+      </div>
+    </>
+  )
+}
