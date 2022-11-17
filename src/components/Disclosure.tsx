564036b409997dcd57d1acc2378371c2bd43f7a8
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2022-11-17 14:59:28 +0100

    fix: prevent unexpected form submit

--- src/components/Disclosure.tsx
+++ src/components/Disclosure.tsx
@@ -13,6 +13,7 @@ export function Disclosure({ title, children }: DisclosureType) {
   return (
     <>
       <button
+        type="button"
         className="flex w-full justify-between rounded-md bg-slate-100 px-4 py-2 text-left font-medium text-slate-900"
         onClick={() => setIsOpen((prev) => !prev)}
       >
