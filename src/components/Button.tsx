fe91deac505a987939ea46fb289b3a310fb7530c
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2022-11-21 15:37:31 +0100

    feat: tag system

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,17 @@
+import { classNames } from 'src/utils'
+
 interface ButtonType extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: string
 }
 
-export function Button({ children, ...args }: ButtonType) {
+export function Button({ children, className, ...args }: ButtonType) {
   return (
     <button
       type="button"
-      className="inline-flex items-center rounded border border-transparent bg-green-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none"
+      className={classNames(
+        'rounded border border-transparent bg-green-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none',
+        className
+      )}
       {...args}
     >
       {children}
