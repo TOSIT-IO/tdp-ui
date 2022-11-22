a32afa2780c37e2a2636c68288c42a647cb123d2
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2022-11-22 17:56:27 +0100

    feat: drag and drop

--- src/pages/deploy/index.tsx
+++ src/pages/deploy/index.tsx
@@ -1,65 +1,23 @@
-import { ReactElement, useRef, useState } from 'react'
+import { ReactElement } from 'react'
 import DashboardLayout from 'src/app/dashboard/layout'
 import { Button } from 'src/components'
 import { useDeploy } from 'src/hooks/useDeploy'
 
+import OperationsList from 'src/app/deploy/OperationsList'
+import InputField from 'src/app/deploy/InputField'
+import { OperationsContextProvider } from 'src/app/deploy/OperationsContext'
+
 const DeployPage = () => {
-  const [targets, setTargets] = useState([])
-  const [input, setInput] = useState('')
-  const { deploy } = useDeploy()
-
-  const inputRef = useRef(null)
-
-  function handleSubmit(event) {
-    event.preventDefault()
-    deploy(targets)
-  }
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col h-screen justify-center p-5 gap-2"
-    >
-      <div className="flex gap-1 flex-wrap">
-        {targets.map((v, i) => (
-          <button
-            key={v}
-            type="button"
-            className="bg-slate-100 rounded-md p-1 border-slate-200 border-[1px]"
-            onClick={(e) => {
-              setInput(targets.splice(i, 1)[0])
-              inputRef.current.focus()
-            }}
-          >
-            {v}
-          </button>
-        ))}
-        <input
-          type="text"
-          ref={inputRef}
-          value={input}
-          style={{ width: `${input.length}ch` }}
-          className="px-2 rounded-md border-slate-200 border-[1px] grow"
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => {
-            const trimmedInput = input.trim()
-            if (
-              [',', 'Enter'].includes(e.key) &&
-              trimmedInput.length &&
-              !targets.includes(trimmedInput)
-            ) {
-              e.preventDefault()
-              setTargets((prev) => [...prev, trimmedInput])
-              setInput('')
-            }
-            if (e.key === 'Backspace' && !input.length && targets.length) {
-              e.preventDefault()
-              setInput(targets.pop())
-            }
-          }}
-        />
+    <OperationsContextProvider>
+      <div className="flex flex-col justify-center h-full items-center gap-2">
+        <div className="flex flex-col max-w-xs">
+          <OperationsList />
+          <InputField />
+        </div>
+        <Button>Deploy</Button>
       </div>
-      <Button type="submit">Deploy</Button>
-    </form>
+    </OperationsContextProvider>
   )
 }
 
