c13a861638aa26566b2c0b2c5b02c39a6da11b42
Jean Savy <jean.savy@dgfip.finances.gouv.fr>
2022-11-23 15:18:25 +0100

    feat: input areas=> enlargement, coloring on hover, integers; toggle, column-editable arrays

--- src/components/Services/VariablesDisplay/VariableField.tsx
+++ src/components/Services/VariablesDisplay/VariableField.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import { classNames } from 'src/utils'
+import { Toggle } from '../Toggle'
 import { useVariablesContext } from './VariablesContext'
 
 type VariableFieldType = {
   prop: string
-  value: string | number | boolean
+  value: string | number | boolean | any[]
   parent?: string
 }
 
@@ -13,22 +14,65 @@ export function VariableField({ prop, value, parent }: VariableFieldType) {
   const [error, setError] = useState(false)
   const inputName = parent ? [parent, prop].join('.') : prop
 
-  function handleVariableChange(event: React.ChangeEvent<HTMLInputElement>) {
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     try {
       const newVariable = JSON.parse(event.target.value)
       setError(false)
       if (!parent) {
         setNewVariables((prev: any) => ({ ...prev, [prop]: newVariable }))
-        return
+      } else {
+        setNewVariables((prev: any) => {
+          const data = { ...prev }
+          data[parent] = prev[parent] || {}
+          data[parent][prop] = newVariable
+          // console.log(newVariable)
+          return data
+        })
       }
+    } catch (err) {
+      setError(true)
+    }
+  }
+
+  function handleChecked(event: React.ChangeEvent<HTMLInputElement>) {
+    const newVariable = event.target.checked
+    if (!parent) {
+      setNewVariables((prev: any) => ({ ...prev, [prop]: newVariable }))
+    } else {
       setNewVariables((prev: any) => {
         const data = { ...prev }
+        // Initialization of parent obj
         data[parent] = prev[parent] || {}
         data[parent][prop] = newVariable
         return data
       })
-    } catch (err) {
-      setError(true)
+    }
+  }
+
+  function ArrayList() {
+    if (Array.isArray(value)) {
+      return (
+        <ol className="flex flex-grow flex-col  gap-2">
+          {value.map((v) => (
+            <li key={inputName} className="flex grow">
+              <input
+                key={inputName}
+                name={inputName}
+                className={classNames(
+                  'grow',
+                  error && 'bg-red-200',
+                  typeof value === 'number'
+                    ? 'text-teal-600'
+                    : 'text-slate-700',
+                  'hover:opacity-60 hover:bg-sky-300 transition duration-300 ease-in-out'
+                )}
+                defaultValue={JSON.stringify(v)}
+                onChange={handleChange}
+              />
+            </li>
+          ))}
+        </ol>
+      )
     }
   }
 
@@ -37,12 +81,34 @@ export function VariableField({ prop, value, parent }: VariableFieldType) {
       <label htmlFor={inputName} className="font-bold mr-2">
         {prop}:
       </label>
-      <input
-        name={inputName}
-        className={classNames('grow', error && 'bg-red-200')}
-        defaultValue={JSON.stringify(value)}
-        onChange={handleVariableChange}
-      />
+      {/* {Array.isArray(value) &&
+        <ol className='flex flex-grow flex-col  gap-2'>
+          {value.map(v => <li className='flex grow'>
+            <input name={inputName} className={classNames('grow', error && 'bg-red-200', typeof value === 'number' ? "text-teal-600" : "text-slate-700","hover:opacity-60 hover:bg-sky-300 transition duration-300 ease-in-out")} defaultValue={JSON.stringify(v)} onChange={handleChange}
+          />
+          </li>)}
+        </ol>
+      } */}
+      {ArrayList()}
+      {typeof value === 'boolean' && (
+        <div className="flex flex-grow flex-col">
+          <Toggle handleChecked={handleChecked} defaultValue={value} />
+        </div>
+      )}
+      {(typeof value === 'string' || typeof value === 'number') && (
+        <input
+          key={inputName}
+          name={inputName}
+          className={classNames(
+            'grow',
+            error && 'bg-red-200',
+            typeof value === 'number' ? 'text-teal-600' : 'text-slate-700',
+            'hover:opacity-60 hover:bg-sky-300 transition duration-300 ease-in-out'
+          )}
+          defaultValue={JSON.stringify(value)}
+          onChange={handleChange}
+        />
+      )}
     </div>
   )
 }
---
+++ src/components/Toggle.tsx
@@ -0,0 +1,15 @@
+export function Toggle({ handleChecked, defaultValue }) {
+  return (
+    <>
+      <label className="inline-flex relative items-center cursor-pointer hover:opacity-60 hover:bg-sky-300 transition duration-300 ease-in-out">
+        <input
+          type="checkbox"
+          className="sr-only peer"
+          onChange={handleChecked}
+          defaultChecked={defaultValue}
+        />
+        <div className="w-11 h-6 bg-gray-200 rounded-full peer  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-indigo-600"></div>
+      </label>
+    </>
+  )
+}
