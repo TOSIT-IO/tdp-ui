1db3081af388453cc1cba98fa01c0ff96f9e79a7
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2022-11-24 16:03:45 +0100

    feat: style input field

--- src/app/deploy/InputField.tsx
+++ src/app/deploy/InputField.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useOperationsContext } from './OperationsContext'
+import { PlusIcon } from '@heroicons/react/24/solid'
 
 export default function InputField() {
   const [inputValue, setInputValue] = useState('')
@@ -11,14 +12,31 @@ export default function InputField() {
     setInputValue('')
   }
   return (
-    <form onSubmit={handleOnSubmit} className="flex">
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        className="flex-grow"
-      />
-      <button type="submit">Add</button>
+    <form onSubmit={handleOnSubmit}>
+      <label
+        htmlFor="email"
+        className="block text-sm font-medium text-gray-700"
+      >
+        Add new operation
+      </label>
+      <div className="mt-1 flex rounded-md shadow-sm">
+        <div className="relative flex flex-grow items-stretch focus-within:z-10">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className="block w-full rounded-none rounded-l-md border border-gray-300 pl-2"
+            placeholder="Operation"
+          />
+        </div>
+        <button
+          type="submit"
+          className="relative -ml-px inline-flex items-center space-x-2 rounded-r-md border border-gray-300 bg-gray-50 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100"
+        >
+          <PlusIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          <span>Add</span>
+        </button>
+      </div>
     </form>
   )
 }
