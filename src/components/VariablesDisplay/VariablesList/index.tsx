a84cdd8d44c2c39b7be0dd18e34b5e8875626d51
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2022-12-06 15:49:41 +0100

    refactor: variableField definition

--- src/components/VariablesDisplay/VariablesList/index.tsx
+++ src/components/VariablesDisplay/VariablesList/index.tsx
@@ -1,4 +1,5 @@
-import { VariableField } from './VariableField'
+import { ViewValue } from './VariableField'
+import { Raw } from './VariableField/Raw'
 
 type VariablesListType = {
   isRaw: boolean
@@ -7,20 +8,32 @@ type VariablesListType = {
 }
 
 export function VariablesList({ isRaw, variables, parent }: VariablesListType) {
-  if (variables.length) {
-    return (
-      <>
-        {variables.map(([k, v]) => (
-          <VariableField
-            key={k}
-            isRaw={isRaw}
-            prop={k}
-            value={v}
-            parent={parent}
-          />
-        ))}
-      </>
-    )
-  }
-  return <p className="text-slate-600">No value</p>
+  if (!variables.length) return <p className="text-slate-600">No value</p>
+
+  return (
+    <>
+      {isRaw ? (
+        variables.map(([k, v]) => (
+          <div key={k} className="flex flex-col gap-2">
+            <Raw prop={k} value={v} parent={parent} />
+          </div>
+        ))
+      ) : (
+        <table className="w-full">
+          <tbody className="flex flex-col gap-1 text-base divide-y">
+            {variables.map(([k, v]) => (
+              <tr key={k} className="flex">
+                <td className="w-[20%] font-bold text-gray-600 overflow-auto">
+                  {k}:
+                </td>
+                <td className="flex-grow text-gray-600">
+                  <ViewValue prop={k} value={v} parent={parent} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  )
 }
