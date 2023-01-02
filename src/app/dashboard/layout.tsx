ded7ead24415fc722f69ac499eb4cff8ada401b3
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2023-01-02 13:49:05 +0100

    refactor: isolate ToastProvider component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,28 +1,17 @@
 import Image from 'next/image'
-import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { Menu, Topbar } from 'src/components/Layout'
-import { Sidebar } from 'src/components/Layout/primitives/Sidebar'
+import { ToastProvider } from 'src/components/Layout/context'
+import { Sidebar } from 'src/components/Layout/primitives'
 
 export default function DashboardLayout({ children }) {
   const [logoInitialWidth, logoInitialHeight] = [126, 50]
   const logoRatio = 0.8
 
   return (
-    <>
-      <ToastContainer
-        position="bottom-center"
-        autoClose={5000}
-        hideProgressBar
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
+    <ToastProvider>
       <Sidebar sideWidth="13rem" space="0">
+        {/* Sidebar */}
         <aside className="h-screen overflow-auto bg-gray-900 flex flex-col">
           <div className="mb-3 py-3 self-center">
             <p className="flex">
@@ -37,11 +26,12 @@ export default function DashboardLayout({ children }) {
           </div>
           <Menu />
         </aside>
+        {/* Main content */}
         <div className="h-screen overflow-y-auto">
           <Topbar className="bg-gray-900 sticky top-0" />
           <main className="p-5">{children}</main>
         </div>
       </Sidebar>
-    </>
+    </ToastProvider>
   )
 }
---
+++ src/components/Layout/context/ToastProvider.tsx
@@ -0,0 +1,21 @@
+import { ToastContainer } from 'react-toastify'
+
+export function ToastProvider({ children }) {
+  return (
+    <>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      {children}
+    </>
+  )
+}
