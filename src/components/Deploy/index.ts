4df607ed6f294b0af4e6e35c3771534a6185bee2
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2023-01-17 15:46:41 +0100

    refactor: isolate deploy preview component

--- src/components/Deploy/index.ts
+++ src/components/Deploy/index.ts
@@ -1,3 +1,4 @@
 export { DeployDag } from './DeployDag'
 export { DeployOperations } from './DeployOperations'
 export { DeployLogs } from './DeployLogs'
+export { DeployPreview } from './DeployPreview'
