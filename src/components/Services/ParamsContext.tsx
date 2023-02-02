c56cd6678b508a3798539bf82c5cc248956a43bf
Paul Farault <diode-farault.consultant@dgfip.finances.gouv.fr>
2023-02-02 21:23:25 +0100

    fix: handle service change

--- src/components/Services/ParamsContext.tsx
+++ src/components/Services/ParamsContext.tsx
@@ -1,4 +1,6 @@
-import { createContext } from 'react'
+import { createContext, useEffect } from 'react'
+import { clearUserInput } from 'src/features/userInput'
+import { useAppDispatch } from 'src/store'
 
 export const ParamsContext = createContext<{
   serviceId: string
@@ -6,6 +8,12 @@ export const ParamsContext = createContext<{
 }>(null)
 
 export function ParamsContextProvider({ children, serviceId, componentId }) {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(clearUserInput())
+  }, [serviceId, dispatch])
+
   return (
     <ParamsContext.Provider value={{ serviceId, componentId }}>
       {children}
