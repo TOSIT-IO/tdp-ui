a04925e2b6e9addcc6eebb2a80cfe44ab9c8287b
Jean Savy <jean.savy@dgfip.finances.gouv.fr>
2023-02-13 16:08:46 +0100

    feat: rename

--- src/components/Layout/LegalNotice.tsx
+++ src/components/Layout/LegalNotice.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export function LegalNotice() {
   const currentYear = new Date().getFullYear()
 
-  const tositUrl = (
+  const tositLink = (
     <Link
       className="text-gray-600 hover:underline"
       href={'https://github.com/TOSIT-IO'}
@@ -23,7 +23,7 @@ export function LegalNotice() {
 
   return (
     <p className="p-5 text-gray-500 text-sm text-center">
-      &copy; {currentYear} {tositUrl} | License {apacheLink}
+      &copy; {currentYear} {tositLink} | License {apacheLink}
     </p>
   )
 }
