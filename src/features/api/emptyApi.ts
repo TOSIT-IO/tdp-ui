35127d4ba0821243fadfdd7b53098481bcdbf38f
Sergei Kudinov <sergkudinov@gmail.com>
2023-04-19 16:31:37 +0200

    refactor: use apiBasePath from redux store in RTK queries

--- src/features/api/emptyApi.ts
+++ src/features/api/emptyApi.ts
@@ -1,5 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import type {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+} from '@reduxjs/toolkit/query/react'
 import { User } from 'oidc-client-ts'
+
 import type { RootState } from 'src/store'
 import type { Config } from 'src/features/config'
 
@@ -16,24 +22,55 @@ const getUser = async (oidc: Config['oidc']) => {
   return User.fromStorageString(oidcStorage)
 }
 
+const rawBaseQuery = fetchBaseQuery({
+  prepareHeaders: async (headers, { getState }) => {
+    // provision with a Bearer token if OIDC is configured
+    const {
+      config: {
+        value: { oidc },
+      },
+    }: Partial<RootState> = getState()
+    if (oidc) {
+      const user = await getUser(oidc)
+      const token = user?.access_token
+      if (token) headers.set('authorization', `Bearer ${token}`)
+    }
+    return headers
+  },
+})
+
+const dynamicBaseQuery: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  let {
+    config: {
+      value: { apiBasePath },
+    },
+  }: Partial<RootState> = api.getState()
+  // gracefully handle scenarios where data to generate the URL is missing
+  if (!apiBasePath) {
+    return {
+      error: {
+        status: 400,
+        statusText: 'Bad Request',
+        data: 'No apiBasePath received',
+      },
+    }
+  }
+  apiBasePath = apiBasePath.replace(/\/*$/, '') // replace "/" at the end if exists
+  const urlEnd = typeof args === 'string' ? args : args.url
+  // construct a dynamically generated portion of the url
+  const adjustedUrl = `${apiBasePath}${urlEnd}`
+  const adjustedArgs =
+    typeof args === 'string' ? adjustedUrl : { ...args, url: adjustedUrl }
+  // provide the amended url and other params to the raw base query
+  return rawBaseQuery(adjustedArgs, api, extraOptions)
+}
+
 // initialize an empty api service that we'll inject endpoints into later as needed
 export const emptyApi = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_TDP_API_URL,
-    prepareHeaders: async (headers, { getState }) => {
-      // provision with a Bearer token if OIDC is configured
-      const {
-        config: {
-          value: { oidc },
-        },
-      }: Partial<RootState> = getState()
-      if (oidc) {
-        const user = await getUser(oidc)
-        const token = user?.access_token
-        if (token) headers.set('authorization', `Bearer ${token}`)
-      }
-      return headers
-    },
-  }),
+  baseQuery: dynamicBaseQuery,
   endpoints: () => ({}),
 })
