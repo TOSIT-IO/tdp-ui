f4f02ec114a0f22f44cb173c4ecea0a08e8feb52
Sergei Kudinov <sergkudinov@gmail.com>
2023-05-03 18:27:06 +0200

    fix: improve toolbar behavior

--- src/components/Services/ServiceVariables/Editor/Toolbar.tsx
+++ src/components/Services/ServiceVariables/Editor/Toolbar.tsx
@@ -1,49 +1,37 @@
 import { Bars3CenterLeftIcon, EyeIcon } from '@heroicons/react/24/solid'
 
 import { IconButon } from 'src/components/commons'
+import { useAppSelector, useAppDispatch } from 'src/store'
+import { setRawMode } from 'src/store/userInput'
 
-const DisplayModeButton = ({
-  isRawMode,
-  showRawMode,
-  showVisualMode,
-}: {
-  isRawMode: boolean
-  showRawMode: () => void
-  showVisualMode: () => void
-}) => (
-  <div className="inline-flex overflow-hidden rounded-md border border-gray-400">
-    <IconButon
-      onClick={showRawMode}
-      isActive={isRawMode}
-      icon={Bars3CenterLeftIcon}
-      text="Raw"
-    />
-    <IconButon
-      onClick={showVisualMode}
-      isActive={!isRawMode}
-      className="-ml-px border-l border-l-gray-400"
-      icon={EyeIcon}
-      text="View"
-    />
-  </div>
-)
-
-const Toolbar = ({
-  isRawMode,
-  showRawMode,
-  showVisualMode,
-}: {
-  isRawMode: boolean
-  showRawMode: () => void
-  showVisualMode: () => void
-}) => (
-  <div className="mb-4 flex justify-end ">
-    <DisplayModeButton
-      isRawMode={isRawMode}
-      showRawMode={showRawMode}
-      showVisualMode={showVisualMode}
-    />
-  </div>
-)
+const Toolbar = () => {
+  const {
+    settings: { showRawMode },
+  } = useAppSelector((state) => state.userInput)
+  const dispatch = useAppDispatch()
+  return (
+    <div className="mb-4 flex justify-end ">
+      <div className="inline-flex overflow-hidden rounded-md border border-gray-400">
+        <IconButon
+          onClick={() => {
+            dispatch(setRawMode(true))
+          }}
+          isActive={showRawMode}
+          icon={Bars3CenterLeftIcon}
+          text="Raw"
+        />
+        <IconButon
+          onClick={() => {
+            dispatch(setRawMode(false))
+          }}
+          isActive={!showRawMode}
+          className="-ml-px border-l border-l-gray-400"
+          icon={EyeIcon}
+          text="View"
+        />
+      </div>
+    </div>
+  )
+}
 
 export default Toolbar
